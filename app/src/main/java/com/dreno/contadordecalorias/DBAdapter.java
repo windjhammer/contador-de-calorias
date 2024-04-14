fd122918f73b7f97aa653dca247f2fc469419ef4
Breno <fernandesopa@gmail.com>
2024-04-14 01:26:11 -0300

    criação da tabela categorias

--- app/src/main/java/com/dreno/contadordecalorias/DBAdapter.java
+++ app/src/main/java/com/dreno/contadordecalorias/DBAdapter.java
@@ -7,10 +7,9 @@ import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-
 public class DBAdapter {
     private static final String databaseName = "calorias";
-    private static final int databaseVersion = 8;
+    private static final int databaseVersion = 9;
 
     private final Context context;
     private DatabaseHelper DBHelper;
@@ -20,72 +19,80 @@ public class DBAdapter {
         this.context = ctx;
         DBHelper = new DatabaseHelper(context);
     }
-        private static class DatabaseHelper extends SQLiteOpenHelper{
-            DatabaseHelper(Context context){
-                super(context, databaseName, null, databaseVersion);
-            }
 
-            @Override
-            public void onCreate(SQLiteDatabase db){
-                try {
-                    //criação das tabelas
-                    db.execSQL("CREATE TABLE IF NOT EXISTS comidas " +
-                            " (comida_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                            " comida_nome VARCHAR," +
-                            " comida_produtor VARCHAR," +
-                            " comida_porcao DOUBLE," +
-                            " comida_medidor_de_porcao VARCHAR," +
-                            " comida_num_porcoes DOUBLE," +
-                            " comida_num_porcoes_nome VARCHAR," +
-                            " comida_calorias DOUBLE," +
-                            " comida_proteina DOUBLE," +
-                            " comida_carboidrato DOUBLE," +
-                            " comida_gordura DOUBLE," +
-                            " comida_calorias_calculado DOUBLE," +
-                            " comida_proteina_calculado DOUBLE," +
-                            " comida_carboidrato_calculado DOUBLE," +
-                            " comida_gordura_calculado DOUBLE," +
-                            " comida_usuario_id INT," +
-                            " comida_codigo_de_barras VARCHAR," +
-                            " comida_categoria_id INT," +
-                            " comida_miniatura VARCHAR," +
-                            " comida_imagem_a VARCHAR," +
-                            " comida_imagem_b VARCHAR," +
-                            " comida_imagem_c VARCHAR," +
-                            " comida_nota VARCHAR);");
-                }
-                catch (SQLException e){
-                    e.printStackTrace();
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+        DatabaseHelper(Context context) {
+            super(context, databaseName, null, databaseVersion);
+        }
 
-                }
-            }
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            try {
+                // criação das tabelas
 
-
-            @Override
-            public void onUpgrade (SQLiteDatabase db, int oldVersion, int newVersion){
-                //Tabelas a serem dropadas
-                db.execSQL("DROP TABLE IF EXISTS comidas");
-                onCreate(db);
-
-                String TAG = "Tag";
-                Log.w(TAG, "Atualizando o banco de dados da versão: " + oldVersion + " para a versão: "
-                        + newVersion + " , o que resultará na destruição de todos os dados.");
+                db.execSQL("CREATE TABLE IF NOT EXISTS categorias (" +
+                        " categoria_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                        " categoria_nome VARCHAR," +
+                        " categoria_parent_id INT," +
+                        " categoria_icone VARCHAR," +
+                        " categoria_nota VARCHAR);");
+                db.execSQL("CREATE TABLE IF NOT EXISTS comidas " +
+                        " (comida_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        " comida_nome VARCHAR," +
+                        " comida_produtor VARCHAR," +
+                        " comida_porcao DOUBLE," +
+                        " comida_medidor_de_porcao VARCHAR," +
+                        " comida_num_porcoes DOUBLE," +
+                        " comida_num_porcoes_nome VARCHAR," +
+                        " comida_calorias DOUBLE," +
+                        " comida_proteina DOUBLE," +
+                        " comida_carboidrato DOUBLE," +
+                        " comida_gordura DOUBLE," +
+                        " comida_calorias_calculado DOUBLE," +
+                        " comida_proteina_calculado DOUBLE," +
+                        " comida_carboidrato_calculado DOUBLE," +
+                        " comida_gordura_calculado DOUBLE," +
+                        " comida_usuario_id INT," +
+                        " comida_codigo_de_barras VARCHAR," +
+                        " comida_categoria_id INT," +
+                        " comida_miniatura VARCHAR," +
+                        " comida_imagem_a VARCHAR," +
+                        " comida_imagem_b VARCHAR," +
+                        " comida_imagem_c VARCHAR," +
+                        " comida_nota VARCHAR);");
+            } catch (SQLException e) {
+                e.printStackTrace();
 
             }
         }
-        public DBAdapter open() throws SQLException{
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            // Tabelas a serem dropadas
+            db.execSQL("DROP TABLE IF EXISTS comidas");
+            onCreate(db);
+
+            String TAG = "Tag";
+            Log.w(TAG, "Atualizando o banco de dados da versão: " + oldVersion + " para a versão: "
+                    + newVersion + " , o que resultará na destruição de todos os dados.");
+
+        }
+    }
+
+    public DBAdapter open() throws SQLException {
         db = DBHelper.getWritableDatabase();
         return this;
     }
+
     public void close() {
         DBHelper.close();
     }
 
-    public void insert(String table, String fields, String values){
+    public void insert(String table, String fields, String values) {
         db.execSQL("INSERT INTO " + table + "(" + fields + ") VALUES (" + values + ")");
     }
 
-    public int contadorDeNotasGravadas (String table){
+    public int contadorDeNotasGravadas(String table) {
         Cursor mCount = db.rawQuery("SELECT COUNT(*) FROM " + table + "", null);
         mCount.moveToFirst();
         int count = mCount.getInt(0);
