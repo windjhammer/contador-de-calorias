164a6746c5ce6af384816e1b9edcd03509f025a6
Breno <fernandesopa@gmail.com>
2024-04-14 17:51:09 -0300

    tela de cadastro continuação

--- app/src/main/java/com/dreno/contadordecalorias/MainActivity.java
+++ app/src/main/java/com/dreno/contadordecalorias/MainActivity.java
@@ -1,6 +1,8 @@
 package com.dreno.contadordecalorias;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.sax.StartElementListener;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -31,6 +33,14 @@ public class MainActivity extends AppCompatActivity {
             setupInsert.insertAllCategorias();
             Toast.makeText(this, "Carregado.", Toast.LENGTH_LONG).show();
 
+        }
+
+        //checar se o usuário está logado
+        numberRows = db.contadorDeNotasGravadas("usuarios");
+        if(numberRows < 1){
+            Toast.makeText(this, "precisa logar", Toast.LENGTH_LONG).show();
+            Intent i = new Intent(MainActivity.this, SignUp.class);
+            startActivity(i);
         }
 
         // db.close();
--- app/src/main/java/com/dreno/contadordecalorias/SignUp.java
+++ app/src/main/java/com/dreno/contadordecalorias/SignUp.java
@@ -1,4 +1,14 @@
 package com.dreno.contadordecalorias;
 
-public class SignUp {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUp extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+
+    }
 }
